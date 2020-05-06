62b54a82d20ef740d68f7fd7b44930fd23a2711e
Sergio Majé <s.maje@udla.edu.co>
2020-05-06 02:46:26 -0500

    Guardar datos en Firebase

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,6 +10,7 @@
         >
       </v-flex>
     </v-layout>
+    <br>
     <v-layout xs12 pb-3>
       <v-flex>
         <v-btn
@@ -18,15 +19,59 @@
         >Enviar</v-btn>
       </v-flex>
     </v-layout>
+    <v-divider></v-divider>
+    <v-layout
+      row
+      wrap
+      align-center
+    >
+      <v-flex xs12>
+        <v-container
+          fluid
+          class="pl-0 pr-0"
+        >
+          <v-layout row wrap>
+            <v-flex 
+              xs12
+              sm6
+              lg3
+              v-for="image in images"
+              v-bind:key="image.id"
+            >
+              <v-card>
+                <v-img
+                  class="white--text"
+                  height="200px"
+                  :src="image.id"
+                >
+                </v-img>
+                <v-card-title>
+                  <div>
+                    <span class="grey--text"> {{image.name}} </span>
+                    <v-chip> {{image.scorePromedio}} </v-chip>
+                    <br>
+                  </div>
+                </v-card-title>
+                <v-card-actions>
+                  <v-btn flat @click="goToImageDetail($image.id)">Explore</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
+import { firestore } from '@/main'
 import { storage } from '@/main'
   export default {
     name: 'Home',
     data: () => ({
-      file: ''
+      file: '',
+      images: []
     }),
     methods: {
       processFile: function(event) {
@@ -37,7 +82,27 @@ import { storage } from '@/main'
         let metadata = { contentType: this.file.type }
         storage.child(name).put(this.file, metadata)
         .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(url => {
+          let image = {
+            url: url,
+            name: name,
+            createdAt: (+new Date()),
+            scorePromedio: 0
+          }
+          firestore.collection('images').add(image)
+        })
+      },
+      goToImageDetail: function(id) {
+        let id1 = id
+        this.$router.push({ path: '/image/${id1}'})
+      }
+    },
+    firestore() {
+      return {
+        // Queries de las bases de datos
+        images: firestore.collection('images')
       }
     }
+    
   }
 </script>
