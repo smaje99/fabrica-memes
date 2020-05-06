620803b2dece38e89bdc8c8b6b41f6fd4611fe0e
Sergio Majé <s.maje@udla.edu.co>
2020-05-06 01:34:40 -0500

    Configurando el proyecto

--- src/App.vue
+++ src/App.vue
@@ -38,23 +38,22 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
+import { firestorePlugin } from 'vuefire'
+import router from './router/index'
 
 Vue.config.productionTip = false
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCHdLrMnyrRExbj5E-3qUSG4ay7A88htTs",
+  authDomain: "fabrica-memes-a7f72.firebaseapp.com",
+  databaseURL: "https://fabrica-memes-a7f72.firebaseio.com",
+  projectId: "fabrica-memes-a7f72",
+  storageBucket: "fabrica-memes-a7f72.appspot.com",
+  messagingSenderId: "26830875393",
+  appId: "1:26830875393:web:628432a8ffb48a6f290900",
+  measurementId: "G-V7JN6FMZLH"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firestore = firebaseApp.firestore()  // Acceso a la base de datos
+export const storage = firebase.storage().ref('/images') 
+
+Vue.use(firestorePlugin)
+
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  router,
+  components: { App }
 }).$mount('#app')
